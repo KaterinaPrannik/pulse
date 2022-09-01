7b8cee95442755bfad9080011441820d561fc92d
Kate <kate199456@tut.by>
2022-09-01 13:53:57 +0300

    add footer and animation

--- src/js/script.js
+++ src/js/script.js
@@ -51,4 +51,47 @@ $(document).ready(function(){
       })
     })
   
+     //Modal
+    $('[data-modal=consultation]').on('click',function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+      $('.overlay,#consultation,#order,#thanks').fadeOut('slow');
+    });
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+          $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+          $('.overlay, #order').fadeIn('slow');
+        });
+    });
+
+    $('#consultation-form').validate();
+    $('#consultation form').validate({
+       rules: {
+        name: 'required',
+        phone: 'required',
+        email: {
+          required: true,
+          email: true
+        }
+       }
+    });
+    $('#order form').validate();
+
+    //smooth scroll
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 1600) {
+        $('.pageup').fadeIn();
+      } else {
+        $('.pageup').fadeOut();
+      }
+      });
+
+      $("a[href^='#']").click(function() {
+        const _href = $(this).attr("href");
+        $("html,body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+      });
   });
+
+ 
